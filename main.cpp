d2e70de7b60b0c8876c0bb5eec7fdc0dd5d2046b
jcastillo2583 <bluejccastle@gmail.com>
2019-09-19 08:35:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <fstream>	//read and write to a file
 using namespace std;
 
 int main()
 {
 	//READ IN FILES 
+	string readfile;
+	cout << "Input file to read from: ";
+	cin >> readfile;	//input file to read from
 
 	//OUTPUT FILE
+	ofstream myfile;
+	myfile.open("Lexical Analysis");	//makes file named Lexical Analysis
+	myfile << "LEXICAL ANALYSIS" << endl << endl;
 
 	//SEARCH KEYWORDS
 
@@ -22,3 +29,7 @@ int main()
 	return 0;
 	system("pause");
 }
+
+ string keywords() {
+
+	}
