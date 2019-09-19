966ef056022d18630e22d1eb198602d0ad0c997d
Victoria  Tran <victoria.tran@csu.fullerton.edu>
2019-09-18 21:32:21 -0700

    Added comments for major functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+	//READ IN FILES 
+
+	//OUTPUT FILE
+
+	//SEARCH KEYWORDS
+
+	//SEARCH OPERATORS 
+
+	//SEARCH IDENTIFIERS 
+
+	//SEARCH REAL NUMBERS 
+
+	//SEARCH SEPERATORS 
+
+	//SEARCH COMMENTS 
+
 	return 0;
 	system("pause");
 }
