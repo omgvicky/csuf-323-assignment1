273fb8095acc82300dd5bf4821be1048c4248db6
Victoria  Tran <victoria.tran@csu.fullerton.edu>
2019-09-28 18:40:15 -0700

    Updated Check digit & Added Flag
    isDigit was checking the numbers in the identifiers when it just wanted to check for assigned integer values so we checked if the character before the digit was an alphanumeric character.
    
    Before, ifnum was outputted as an identifier 
    Added a flag to fix

--- main.cpp
+++ main.cpp
@@ -6,97 +6,102 @@
 #include<type_traits>
 
 using namespace std;
+int flag=0;
 
 bool isKeyword(char input[]) {
-	char keyWords[20][10] = { "int", "float", "bool", "if", "else", "then", "endif", "while",
-				"whileend", "do", "doend", "for", "forend", "input", "output",
-				"and", "or", "function", "void", "main" };
-	int i, flag = 0;
-
-	for (i = 0; i < 32; ++i) {
-		if (strcmp(keyWords[i], input) == 0) {
-			return true;
-		}
-	}
-
-	return false;
+    char keyWords[20][10] = { "int", "float", "bool", "if", "else", "then", "endif", "while",
+        "whileend", "do", "doend", "for", "forend", "input", "output",
+        "and", "or", "function", "void", "main" };
+    int i;
+    
+    for (i = 0; i < 32; ++i) {
+        if (strcmp(keyWords[i], input) == 0) {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 int main() {
-	char testChar, testWord[20], operators[] = "+-*/%=", separators[] = "'(){}[],.:;!";
-	ifstream file("Sample Input File.txt");
-	int i, j = 0;
-
-	ofstream myfile;
-	myfile.open("Lexical Analysis");	//makes file named Lexical Analysis
-	myfile << "LEXICAL ANALYSIS" << endl << endl;
-
-	if (!file.is_open()) {
-		cout << "error while opening the file\n";
-		exit(0);
-	}
-
-	while (!file.eof())
-	{
-		//We get the first character of each word
-		testChar = file.get();
-
-		//check if comment
-		if (testChar == '!') {
-			testChar = file.get();
-			while (testChar != '!') {
-				testChar = file.get();
-			}
-		}
-
-		//Check operator
-		for (i = 0; i < 6; ++i)
-		{
-			if (testChar == operators[i]) {
-				cout << testChar << " is operator\n";
-				myfile << "OPERATOR:      " << testChar << endl;
-			}
-		}
-		//Check seprator
-		for (i = 0; i < 11; ++i)
-		{
-			if (testChar == separators[i]) {
-				cout << testChar << " is separator\n";
-				myfile << "SEPARATOR:     " << testChar << endl;
-			}
-		}
-		//Check if number is real or integer??????
-		if (isdigit(testChar))
-		{
-			cout << testChar << " is a number\n";
-			myfile << "NUMBER:        " << testChar << endl;
-		}
-
-		//If the first character is not operator nor the seperator, get all the rest of word until reach space
-		if (isalnum(testChar)|| testChar=='$')
-		{
-			testWord[j++] = testChar;
-		}
-		else if ((testChar == ' ' || testChar == '\n') && (j != 0))
-		{
-			//Make the rest of c string become null
-			testWord[j] = '\0';
-			//reset new index of testWord for next testWord
-			j = 0;
-
-			if (isKeyword(testWord)) {
-				cout << testWord << " is keyword\n";
-				myfile << "KEYWORD:       " << testWord << endl;
-			}
-			else {
-				cout << testWord << " is identifier\n";
-				myfile << "IDENTIFIER:    " << testWord << endl;
-			}
-		}
-	}
-
-	file.close();
-	myfile.close();
-
-	return 0;
+    char testChar, testWord[20], operators[] = "+-*/%=", separators[] = "'(){}[],.:;!";
+    ifstream file("SampleInputFile.txt");
+    int i, j = 0;
+    
+    ofstream myfile;
+    myfile.open("Lexical Analysis");    //makes file named Lexical Analysis
+    myfile << "LEXICAL ANALYSIS" << endl << endl;
+    
+    if (!file.is_open()) {
+        cout << "error while opening the file\n";
+        exit(0);
+    }
+    
+    while (!file.eof())
+    {
+        //We get the first character of each word
+        testChar = file.get();
+        
+        //check if comment
+        if (testChar == '!') {
+            testChar = file.get();
+            while (testChar != '!') {
+                testChar = file.get();
+            }
+        }
+        
+        //Check operator
+        for (i = 0; i < 6; ++i)
+        {
+            if (testChar == operators[i]) {
+                cout << testChar << " is operator\n";
+                myfile << "OPERATOR:      " << testChar << endl;
+                flag=1;
+            }
+        }
+        //Check seprator
+        for (i = 0; i < 11; ++i)
+        {
+            if (testChar == separators[i]) {
+                cout << testChar << " is separator\n";
+                myfile << "SEPARATOR:     " << testChar << endl;
+                 flag=1;
+            }
+        }
+        //Check if number is real or integer??????
+        if (isdigit(testChar) && !isalnum(testChar-1))
+        {
+            cout << testChar << " is a number\n";
+            myfile << "NUMBER:        " << testChar << endl;
+        }
+        
+        //If the first character is not operator nor the seperator, get all the rest of word until reach space
+        if (isalnum(testChar)|| testChar=='$')
+        {
+            testWord[j++] = testChar;
+        }
+        else if ((testChar == ' ' || flag == 1 || testChar == '\n') && (j != 0))
+        {
+            //Make the rest of c string become null
+            testWord[j] = '\0';
+            //reset new index of testWord for next testWord
+            j = 0;
+            
+            if (isKeyword(testWord)) {
+                cout << testWord << " is keyword\n";
+                myfile << "KEYWORD:       " << testWord << endl;
+            }
+            else {
+                cout << testWord << " is identifier\n";
+                myfile << "IDENTIFIER:    " << testWord << endl;
+            }
+            
+            flag = 0;
+        }
+    }
+    
+    file.close();
+    myfile.close();
+    
+    return 0;
 }
